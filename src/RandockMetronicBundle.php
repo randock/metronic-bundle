8a4edd6952c3c12e3eac0475001958da0f31c8aa
Diego Hervas Ruiz <diego.diguin91@gmail.com>
2017-04-26 13:14:09 +0200

    Initial commit

---
+++ src/RandockMetronicBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Randock\RandockMetronicBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RandockMetronicBundle.
+ */
+class RandockMetronicBundle extends Bundle
+{
+}
