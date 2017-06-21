8880a896e2ad8d6fe7cf3424d42fec441b2379ae
Diego Hervas <diego.diguin91@gmail.com>
2017-06-21 16:50:57 +0200

    Removed unused file and added contstants to MenuItemProvicerInterface

--- src/Menu/Item/MenuItemProviderInterface.php
+++ src/Menu/Item/MenuItemProviderInterface.php
@@ -10,6 +10,15 @@ use Knp\Menu\FactoryInterface;
 interface MenuItemProviderInterface
 {
     /**
+     * Posible values of the option notificationType
+     */
+    public const SUCCESS = 'success';
+    public const WARNING = 'warning';
+    public const DANGER = 'danger';
+    public const INFO = 'info';
+
+
+    /**
      * @param ItemInterface    $menu
      * @param FactoryInterface $factory
      * @param string           $typeMenu
