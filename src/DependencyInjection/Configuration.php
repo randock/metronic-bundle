96fe8cd6e02021d6eeb2815a5e29ce067a56e6e0
Jop Peters <jop@mastercoding.nl>
2017-08-08 14:36:47 +0200

    Allow for menu builder in layout1 and 3

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Randock\MetronicBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('randock_metronic');
+
+        $rootNode
+            ->children()
+            ->enumNode('layout')->values([1,3])->end()
+            ->end()
+            ;
+
+        return $treeBuilder;
+    }
+}
--- src/DependencyInjection/RandockMetronicExtension.php
+++ src/DependencyInjection/RandockMetronicExtension.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Randock\MetronicBundle\DependencyInjection;
 
+use Randock\MetronicBundle\Menu\MenuBuilder;
+use Randock\MetronicBundle\Menu\MenuBuilder1;
+use Randock\MetronicBundle\Menu\MenuBuilder3;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -18,5 +21,16 @@ class RandockMetronicExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $def = $container->getDefinition('metronic.menu_builder');
+        if ($config['layout'] == 1) {
+            $def->setClass(MenuBuilder1::class);
+        } elseif ($config['layout'] == 3) {
+            $def->setClass(MenuBuilder3::class);
+        }
+
     }
 }
