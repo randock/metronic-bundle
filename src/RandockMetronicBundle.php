ae56cdc64ebc327926bacb6bae7867b40a9d5315
Diego Hervas <diego.diguin91@gmail.com>
2018-04-25 16:09:16 +0200

    Webpack and symfony 4 changes

--- src/RandockMetronicBundle.php
+++ src/RandockMetronicBundle.php
@@ -16,81 +16,86 @@ class RandockMetronicBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container->loadFromExtension('assetic', [
-            'assets' => [
-                'randock_metronic_css' => [
-                    'inputs' => [
-                        'bundles/randockmetronic/assets/global/plugins/font-awesome/css/font-awesome.min.css',
-                        'bundles/randockmetronic/assets/global/plugins/simple-line-icons/simple-line-icons.min.css',
-                        'bundles/randockmetronic/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css',
-                        'bundles/randockmetronic/assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css'
-                    ],
-                ],
-                'randock_metronic_scss' => [
-                    'inputs' => [
-                        'bundles/randockmetronic/sass/bootstrap.scss',
-                        'bundles/randockmetronic/sass/global/components-md.scss',
-                        'bundles/randockmetronic/sass/global/plugins-md.scss',
-                    ]
-                ],
-                'randock_metronic_scss_layout1' => [
-                    'inputs' => [
-                        '@randock_metronic_scss',
-                        'bundles/randockmetronic/sass/layouts/layout/layout.scss',
-                        'bundles/randockmetronic/sass/layouts/layout/themes/grey.scss',
-                        'bundles/randockmetronic/sass/layouts/layout/custom.scss'
-                    ]
-                ],
-                'randock_metronic_scss_layout3' => [
-                    'inputs' => [
-                        '@randock_metronic_scss',
-                        'bundles/randockmetronic/sass/layouts/layout3/layout.scss',
-                        'bundles/randockmetronic/sass/layouts/layout3/themes/default.scss',
-                        'bundles/randockmetronic/sass/layouts/layout3/custom.scss'
-                    ]
-                ],
-                'randock_metronic_js' => [
-                    'inputs' => [
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap/js/bootstrap.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/js.cookie.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery.blockui.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/scripts/datatable.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/datatables/datatables.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js',
-                        '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery-validation/js/jquery.validate.js',
 
-                        '@RandockMetronicBundle/Resources/public/assets/global/scripts/app.min.js',
+        if ($container->hasExtension('assetic')) {
+            $container->loadFromExtension('assetic', [
+                    'assets' => [
+                        'randock_metronic_css' => [
+                            'inputs' => [
+                                'bundles/randockmetronic/assets/global/plugins/font-awesome/css/font-awesome.min.css',
+                                'bundles/randockmetronic/assets/global/plugins/simple-line-icons/simple-line-icons.min.css',
+                                'bundles/randockmetronic/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.css',
+                                'bundles/randockmetronic/assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css'
+                            ],
+                        ],
+                        'randock_metronic_scss' => [
+                            'inputs' => [
+                                'bundles/randockmetronic/sass/bootstrap.scss',
+                                'bundles/randockmetronic/sass/global/components-md.scss',
+                                'bundles/randockmetronic/sass/global/plugins-md.scss',
+                            ]
+                        ],
+                        'randock_metronic_scss_layout1' => [
+                            'inputs' => [
+                                '@randock_metronic_scss',
+                                'bundles/randockmetronic/sass/layouts/layout/layout.scss',
+                                'bundles/randockmetronic/sass/layouts/layout/themes/grey.scss',
+                                'bundles/randockmetronic/sass/layouts/layout/custom.scss'
+                            ]
+                        ],
+                        'randock_metronic_scss_layout3' => [
+                            'inputs' => [
+                                '@randock_metronic_scss',
+                                'bundles/randockmetronic/sass/layouts/layout3/layout.scss',
+                                'bundles/randockmetronic/sass/layouts/layout3/themes/default.scss',
+                                'bundles/randockmetronic/sass/layouts/layout3/custom.scss'
+                            ]
+                        ],
+                        'randock_metronic_js' => [
+                            'inputs' => [
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap/js/bootstrap.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/js.cookie.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery.blockui.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-tagsinput/bootstrap-tagsinput.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/scripts/datatable.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/datatables/datatables.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/plugins/jquery-validation/js/jquery.validate.js',
 
-                        '@RandockMetronicBundle/Resources/public/assets/layouts/global/scripts/quick-sidebar.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/layouts/global/scripts/quick-nav.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/global/scripts/app.min.js',
+
+                                '@RandockMetronicBundle/Resources/public/assets/layouts/global/scripts/quick-sidebar.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/layouts/global/scripts/quick-nav.min.js',
+                            ],
+                        ],
+                        'randock_metronic_js_layout3' => [
+                            'inputs' => [
+                                '@randock_metronic_js',
+                                '@RandockMetronicBundle/Resources/public/assets/layouts/layout3/scripts/layout.min.js',
+                                '@RandockMetronicBundle/Resources/public/assets/layouts/layout3/scripts/demo.js'
+                            ]
+                        ],
+                        'randock_metronic_js_layout1' => [
+                            'inputs' => [
+                                '@randock_metronic_js',
+                                '@RandockMetronicBundle/Resources/public/assets/layouts/layout/scripts/layout.min.js'
+                            ]
+                        ]
                     ],
-                ],
-                'randock_metronic_js_layout3' => [
-                    'inputs' => [
-                        '@randock_metronic_js',
-                        '@RandockMetronicBundle/Resources/public/assets/layouts/layout3/scripts/layout.min.js',
-                        '@RandockMetronicBundle/Resources/public/assets/layouts/layout3/scripts/demo.js'
-                    ]
-                ],
-                'randock_metronic_js_layout1' => [
-                    'inputs' => [
-                        '@randock_metronic_js',
-                        '@RandockMetronicBundle/Resources/public/assets/layouts/layout/scripts/layout.min.js'
-                    ]
                 ]
-            ],
-        ]);
+            );
+        }
 
         $container->loadFromExtension('twig', [
-            'paths' => [
-                '%kernel.root_dir%/../vendor/randock/metronic-bundle/src/Resources/views' => 'RandockMetronicBundle',
-            ],
-        ]);
+                'paths' => [
+                    '%kernel.project_dir%/vendor/randock/metronic-bundle/src/Resources/views' => 'RandockMetronicBundle',
+                ],
+            ]
+        );
 
         $container->addCompilerPass(new HeaderListPass());
         $container->addCompilerPass(new MenuPass());
